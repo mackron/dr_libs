ae97d9ca12cf4521f12644dcdcaeaf60d14c92d9
David Reid <mackron@gmail.com>
2015-07-23 18:33:07 +1000

    Add initial API.

---
+++ easy_path.c
@@ -0,0 +1,161 @@
+// Version 0.1 - Public Domain. See "unlicense" statement at the end of this file.
+
+#include "easy_path.h"
+
+easypath_iterator easypath_begin(const char* path)
+{
+    easypath_iterator i;
+    i.path = path;
+    i.segment.offset = 0;
+    i.segment.length = 0;
+
+    return i;
+}
+
+int easypath_next(easypath_iterator* i)
+{
+    if (i != 0 && i->path != 0)
+    {
+        i->segment.offset = i->segment.offset + i->segment.length;
+        i->segment.length = 0;
+
+        while (i->path[i->segment.offset] != '\0' && (i->path[i->segment.offset] == '/' || i->path[i->segment.offset] == '\\'))
+        {
+            i->segment.offset += 1;
+        }
+
+        if (i->path[i->segment.offset] == '\0')
+        {
+            return 0;
+        }
+
+
+
+        while (i->path[i->segment.offset + i->segment.length] != '\0' && (i->path[i->segment.offset + i->segment.length] != '/' && i->path[i->segment.offset + i->segment.length] != '\\'))
+        {
+            i->segment.length += 1;
+        }
+
+        return 1;
+    }
+    
+    return 0;
+}
+
+int easypath_iterators_equal(const easypath_iterator i0, const easypath_iterator i1)
+{
+    return easypath_segments_equal(i0.path, i0.segment, i1.path, i1.segment);
+}
+
+int easypath_segments_equal(const char* s0Path, const easypath_segment s0, const char* s1Path, const easypath_segment s1)
+{
+    if (s0Path != 0 && s1Path != 0)
+    {
+        if (s0.length == s1.length)
+        {
+            for (int i = 0; i < s0.length; ++i)
+            {
+                if (s0Path[s0.offset + i] != s1Path[s1.offset + i])
+                {
+                    return 0;
+                }
+            }
+
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+
+void easypath_toforwardslashes(char* path)
+{
+    if (path != 0)
+    {
+        while (path[0] != '\0')
+        {
+            if (path[0] == '\\')
+            {
+                path[0] = '/';
+            }
+
+            path += 1;
+        }
+    }
+}
+
+void easypath_tobackslashes(char* path)
+{
+    if (path != 0)
+    {
+        while (path[0] != '\0')
+        {
+            if (path[0] == '/')
+            {
+                path[0] = '\\';
+            }
+
+            path += 1;
+        }
+    }
+}
+
+
+int easypath_isdescendant(const char* descendantAbsolutePath, const char* parentAbsolutePath)
+{
+    // The path cannot be a descendant if it is shorter than the absolute path.
+
+    easypath_iterator iParent = easypath_begin(parentAbsolutePath);
+    easypath_iterator iChild  = easypath_begin(descendantAbsolutePath);
+
+    while (easypath_next(&iParent))
+    {
+        if (easypath_next(&iChild))
+        {
+            // If the segment is different, the paths are different and thus it is not a descendant.
+            if (!easypath_iterators_equal(iParent, iChild))
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            // The descendant is shorter.
+            return 0;
+        }
+    }
+
+    // At this point we have finished iteration of the parent, which should be shorter one. We now do one final iteration of
+    // the descendant to ensure it is indeed shorter. If so, it's a descendant.
+    return easypath_next(&iChild);
+}
+
+
+
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+*/
---
+++ easy_path.h
@@ -0,0 +1,118 @@
+// Version 0.1 - Public Domain. See "unlicense" statement at the end of this file.
+
+#ifndef __easy_path_h_
+#define __easy_path_h_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+// Structure representing a section of a path.
+typedef struct 
+{
+    int offset;
+    int length;
+
+} easypath_segment;
+
+// Structure used for iterating over a path while at the same time providing useful and easy-to-use information about the iteration.
+typedef struct easypath_iterator 
+{
+    const char* path;
+    easypath_segment segment;
+
+} easypath_iterator;
+
+
+
+
+/// Compares a section of two strings for equality.
+///
+/// @param s0Path [in] The first path.
+/// @param s0     [in] The segment of the first path to compare.
+/// @param s1Path [in] The second path.
+/// @param s1     [in] The segment of the second path to compare.
+///
+/// @return 1 if the strings are equal; 0 otherwise.
+int easypath_segments_equal(const char* s0Path, const easypath_segment s0, const char* s1Path, const easypath_segment s1);
+
+
+/// Creates an iterator for iterating over each segment in a path.
+///
+/// @param path [in] The path whose segments are being iterated.
+///
+/// @return An iterator object that is passed to easypath_next().
+easypath_iterator easypath_begin(const char* path);
+
+/// Goes to the next segment in the path as per the given iterator.
+///
+/// @param i [in] A pointer to the iterator to increment.
+///
+/// @return True if the iterator contains a valid value. Use this to determine when to terminate iteration.
+int easypath_next(easypath_iterator* i);
+
+/// Compares the string values of two iterators for equality.
+///
+/// @param i0 [in] The first iterator to compare.
+/// @param i1 [in] The second iterator to compare.
+///
+/// @return 1 if the strings are equal; 0 otherwise.
+int easypath_iterators_equal(const easypath_iterator i0, const easypath_iterator i1);
+
+
+/// Converts the slashes in the given path to forward slashes.
+///
+/// @param path [in] The path whose slashes are being converted.
+void easypath_toforwardslashes(char* path);
+
+/// Converts the slashes in the given path to back slashes.
+///
+/// @param path [in] The path whose slashes are being converted.
+void easypath_tobackslashes(char* path);
+
+
+/// Determines whether or not the given path is a decendant of another.
+///
+/// @param descendantAbsolutePath [in] The absolute path of the descendant.
+/// @param parentAbsolutePath     [in] The absolute path of the parent.
+///
+/// @remarks
+///     As an example, "C:/My/Folder" is a descendant of "C:/".
+///     @par
+///     If either path contains "." or "..", clean it with easypath_clean() before calling this.
+int easypath_isdescendant(const char* descendantAbsolutePath, const char* parentAbsolutePath);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+*/
