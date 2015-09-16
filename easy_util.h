7df37a993048dcf04a2d003fd22091238df49d1a
David Reid <mackron@gmail.com>
2015-09-16 21:49:51 +1000

    Initial commit.

---
+++ easy_util.h
@@ -0,0 +1,97 @@
+// Version 0.1 - Public Domain. See "unlicense" statement at the end of this file.
+
+#ifndef easy_util
+#define easy_util
+
+#if !defined(_MSC_VER)
+#include <errno.h>
+#endif
+
+
+/////////////////////////////////////////////////////////
+// Annotations
+
+#ifndef IN
+#define IN
+#endif
+
+#ifndef	OUT
+#define OUT
+#endif
+
+#ifndef UNUSED
+#define UNUSED(x) ((void)x)
+#endif
+
+
+//////////////////////////////////////////////////////////
+// Below are implementations for functions that are
+// specific to MSVC
+
+/// A basic implementation of MSVC's strcpy_s().
+#if !defined(_MSC_VER)
+inline errno strcpy_s(OUT char* dst, size_t dstSizeInBytes, const char* src)
+{
+    if (dst == NULL) {
+        return EINVAL;
+    }
+    if (dstSizeInBytes == 0) {
+        return ERANGE;
+    }
+    if (src == NULL) {
+        dst[0] = '\0';
+        return EINVAL;
+    }
+    
+    char* iDst = dst;
+    const char* iSrc = src;
+    size_t remainingSizeInBytes = dstSizeInBytes;
+	while (remainingSizeInBytes > 0 && iSrc[0] != '\0')
+    {
+        iDst[0] = iSrc[0];
+
+        iDst += 1;
+        iSrc += 1;
+        remainingSizeInBytes -= 1;
+    }
+
+    if (remainingSizeInBytes > 0) {
+        iDst[0] = '\0';
+    } else {
+        dst[0] = '\0';
+        return ERANGE;
+    }
+
+    return 0;
+}
+
+
+
+#endif
+
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+*/
