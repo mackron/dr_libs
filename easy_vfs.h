fc89a495e8d52570587b6663abba0b2da4780fb1
David Reid <mackron@gmail.com>
2015-09-28 14:45:23 +1000

    Change the max path length to something a bit more reasonable.

--- easy_vfs.h
+++ easy_vfs.h
@@ -28,8 +28,8 @@ extern "C" {
 // at 256 is fine, but it's not a problem to increase the size if you are encountering truncation issues. Note that increasing this
 // value will increase memory usage. You should not need make this any higher than 4096.
 //#define EASYVFS_MAX_PATH    256
-//#define EASYVFS_MAX_PATH    1024
-#define EASYVFS_MAX_PATH    4096
+#define EASYVFS_MAX_PATH    1024
+//#define EASYVFS_MAX_PATH    4096
 
 
 
