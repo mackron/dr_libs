dd2b22b7046b3f59b759837cd167967a2eaffe67
David Reid <mackron@gmail.com>
2016-09-03 20:01:21 +1000

    dr.h: Make a function inline.

--- dr.h
+++ dr.h
@@ -159,7 +159,7 @@ void dr_strrmchar(char* str, char c);
 /// Finds the first non-whitespace character in the given string.
 const char* dr_first_non_whitespace(const char* str);
 
-static const char* dr_ltrim(const char* str) { return dr_first_non_whitespace(str); }
+static inline const char* dr_ltrim(const char* str) { return dr_first_non_whitespace(str); }
 static const char* dr_rtrim(const char* str);
 
 /// Trims both the leading and trailing whitespace from the given string.
