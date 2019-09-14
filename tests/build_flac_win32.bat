46c2c6907f7c36a7c7d2de8315b0505e4a6e7da6
David Reid <mackron@gmail.com>
2019-09-15 07:12:15 +1000

    dr_flac: Update the Win32 tests build script.

--- tests/build_flac_win32.bat
+++ tests/build_flac_win32.bat
@@ -1,1 +1,12 @@
-gcc ./flac/dr_flac_test_0.c -o ./bin/dr_flac_test_0.exe -std=c89 -ansi -pedantic -Wall
+@echo off
+SET c_compiler=gcc
+SET cpp_compiler=g++
+SET options=-Wall -Wpedantic -std=c89 -ansi -pedantic -O2 -s
+
+:: Configure the "arch" option to test different instruction sets.
+SET arch=
+::SET arch=-msse4.1
+::SET arch=-mfpu=neon
+@echo on
+
+%c_compiler% ./flac/dr_flac_test_0.c -o ./bin/dr_flac_test_0.exe %options% %arch%
