ce2b862634540d721660b607b34cc79a5d0d76fc
David Reid <mackron@gmail.com>
2019-09-22 17:45:14 +1000

    dr_flac: Update build script for fairer comparisons.

--- tests/build_flac_win32.bat
+++ tests/build_flac_win32.bat
@@ -13,13 +13,13 @@ SET cpp_compiler=g++
 
 :: Configure the "arch" option to test different instruction sets.
 SET arch=
-SET arch=-msse4.1
+SET arch=-msse4.1 -mtune=native
 ::SET arch=-mfpu=neon
 
 :: libFLAC and libogg are required for benchmarking.
 SET libFLAC=-I./flac/include -L./flac/lib/win32 -lFLAC -logg
 
-SET options=-Wall -Wpedantic -std=c89 -ansi -pedantic -O3 -s %arch% %libFLAC%
+SET options=-Wall -Wpedantic -std=c89 -ansi -pedantic -O3 -s -DNDEBUG %arch% %libFLAC%
 @echo on
 
 %c_compiler% ./flac/dr_flac_test_0.c -o ./bin/dr_flac_test_0.exe %options%
