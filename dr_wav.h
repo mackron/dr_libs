ccd234bfd8532e922276cc53a1ec0d4d0603c2a4
David Reid <mackron@gmail.com>
2019-10-04 17:46:22 +1000

    dr_wav: Fix GCC warnings about functions possibly not being inlineable.

--- dr_wav.h
+++ dr_wav.h
@@ -881,13 +881,22 @@ void drwav_close(drwav* pWav);
 #define DRWAV_MAX_SIMD_VECTOR_SIZE         64  /* 64 for AVX-512 in the future. */
 
 #ifdef _MSC_VER
-#define DRWAV_INLINE __forceinline
+    #define DRWAV_INLINE __forceinline
+#elif defined(__GNUC__)
+    /*
+    I've had a bug report where GCC is emitting warnings about functions possibly not being inlineable. This warning happens when
+    the __attribute__((always_inline)) attribute is defined without an "inline" statement. I think therefore there must be some
+    case where "__inline__" is not always defined, thus the compiler emitting these warnings. When using -std=c89 or -ansi on the
+    command line, we cannot use the "inline" keyword and instead need to use "__inline__". In an attempt to work around this issue
+    I am using "__inline__" only when we're compiling in strict ANSI mode.
+    */
+    #if defined(__STRICT_ANSI__)
+        #define DRWAV_INLINE __inline__ __attribute__((always_inline))
+    #else
+        #define DRWAV_INLINE inline __attribute__((always_inline))
+    #endif
 #else
-#ifdef __GNUC__
-#define DRWAV_INLINE __inline__ __attribute__((always_inline))
-#else
-#define DRWAV_INLINE
-#endif
+    #define DRWAV_INLINE
 #endif
 
 #if defined(SIZE_MAX)
