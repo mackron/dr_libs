c42ac66c143442f99db0e4a703b750f456d4d361
David Reid <mackron@gmail.com>
2019-10-05 14:10:55 +1000

    dr_wav: Clarify some documentation.

--- dr_wav.h
+++ dr_wav.h
@@ -707,13 +707,13 @@ Returns the number of bytes actually read.
 size_t drwav_read_raw(drwav* pWav, size_t bytesToRead, void* pBufferOut);
 
 /*
-Reads a chunk of audio data in the native internal format.
+Reads up to the specified number of PCM frames from the WAV file.
 
-This is typically the most efficient way to retrieve audio data, but it does not do any format
-conversions which means you'll need to convert the data manually if required.
+The output data will be in the file's internal format, converted to native-endian byte order. Use
+drwav_read_pcm_frames_s16/f32/s32() to read data in a specific format.
 
 If the return value is less than <framesToRead> it means the end of the file has been reached or
-you have requested more samples than can possibly fit in the output buffer.
+you have requested more PCM frames than can possibly fit in the output buffer.
 
 This function will only work when sample data is of a fixed size and uncompressed. If you are
 using a compressed format consider using drwav_read_raw() or drwav_read_pcm_frames_s16/s32/f32().
@@ -739,6 +739,9 @@ size_t drwav_write_raw(drwav* pWav, size_t bytesToWrite, const void* pData);
 Writes PCM frames.
 
 Returns the number of PCM frames written.
+
+Input samples need to be in native-endian byte order. On big-endian architectures the input data will be converted to
+little-endian. Use drwav_write_raw() to write raw audio data without performing any conversion.
 */
 drwav_uint64 drwav_write_pcm_frames(drwav* pWav, drwav_uint64 framesToWrite, const void* pData);
 
