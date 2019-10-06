612942656d8375367be39395f134d7b857dfaab6
David Reid <mackron@gmail.com>
2019-10-07 08:11:25 +1000

    dr_mp3: Fix a potential warning with GCC.

--- dr_mp3.h
+++ dr_mp3.h
@@ -160,13 +160,22 @@ typedef drmp3_uint32     drmp3_bool32;
 #define DRMP3_MAX_SAMPLES_PER_FRAME         (DRMP3_MAX_PCM_FRAMES_PER_MP3_FRAME*2)
 
 #ifdef _MSC_VER
-#define DRMP3_INLINE __forceinline
+    #define DRMP3_INLINE __forceinline
+#elif defined(__GNUC__)
+    /*
+    I've had a bug report where GCC is emitting warnings about functions possibly not being inlineable. This warning happens when
+    the __attribute__((always_inline)) attribute is defined without an "inline" statement. I think therefore there must be some
+    case where "__inline__" is not always defined, thus the compiler emitting these warnings. When using -std=c89 or -ansi on the
+    command line, we cannot use the "inline" keyword and instead need to use "__inline__". In an attempt to work around this issue
+    I am using "__inline__" only when we're compiling in strict ANSI mode.
+    */
+    #if defined(__STRICT_ANSI__)
+        #define DRMP3_INLINE __inline__ __attribute__((always_inline))
+    #else
+        #define DRMP3_INLINE inline __attribute__((always_inline))
+    #endif
 #else
-#ifdef __GNUC__
-#define DRMP3_INLINE __inline__ __attribute__((always_inline))
-#else
-#define DRMP3_INLINE
-#endif
+    #define DRMP3_INLINE
 #endif
 
 /*
