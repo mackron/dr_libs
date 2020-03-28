7219550c0b940eb1ce8b8799ad443678fafd1174
David Reid <mackron@gmail.com>
2020-03-29 07:07:38 +1000

    dr_opus: Update DROPUS_INLINE.

--- wip/dr_opus.h
+++ wip/dr_opus.h
@@ -82,15 +82,23 @@ typedef void (* dropus_proc)(void);
     #define DROPUS_SIZE_MAX 0xFFFFFFFF  /* When SIZE_MAX is not defined by the standard library just default to the maximum 32-bit unsigned integer. */
 #endif
 
-
 #ifdef _MSC_VER
-#define DROPUS_INLINE __forceinline
+    #define DROPUS_INLINE __forceinline
+#elif defined(__GNUC__)
+    /*
+    I've had a bug report where GCC is emitting warnings about functions possibly not being inlineable. This warning happens when
+    the __attribute__((always_inline)) attribute is defined without an "inline" statement. I think therefore there must be some
+    case where "__inline__" is not always defined, thus the compiler emitting these warnings. When using -std=c89 or -ansi on the
+    command line, we cannot use the "inline" keyword and instead need to use "__inline__". In an attempt to work around this issue
+    I am using "__inline__" only when we're compiling in strict ANSI mode.
+    */
+    #if defined(__STRICT_ANSI__)
+        #define DROPUS_INLINE __inline__ __attribute__((always_inline))
+    #else
+        #define DROPUS_INLINE inline __attribute__((always_inline))
+    #endif
 #else
-#ifdef __GNUC__
-#define DROPUS_INLINE __inline__ __attribute__((always_inline))
-#else
-#define DROPUS_INLINE inline
-#endif
+    #define DROPUS_INLINE
 #endif
 
 typedef int dropus_result;
