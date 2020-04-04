10d6a2ef44874a93c9f2a7579ae3157c0c83f0b6
David Reid <mackron@gmail.com>
2020-04-04 11:10:39 +1000

    dr_mp3: Version 0.6.0

--- dr_mp3.h
+++ dr_mp3.h
@@ -1,6 +1,6 @@
 /*
 MP3 audio decoder. Choice of public domain or MIT-0. See license statements at the end of this file.
-dr_mp3 - v0.6.0 - TBD
+dr_mp3 - v0.6.0 - 2020-04-04
 
 David Reid - mackron@gmail.com
 
@@ -4335,7 +4335,7 @@ counts rather than sample counts.
 /*
 REVISION HISTORY
 ================
-v0.6.0 - TBD
+v0.6.0 - 2020-04-04
   - API CHANGE: Remove the pConfig parameter from the following APIs:
     - drmp3_init()
     - drmp3_init_memory()
