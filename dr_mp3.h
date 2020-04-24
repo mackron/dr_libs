fbb9d9e8a4520ea04a5767ce95c82a0c7916658a
David Reid <mackron@gmail.com>
2020-04-25 09:03:10 +1000

    Update revision history.

--- dr_mp3.h
+++ dr_mp3.h
@@ -1,6 +1,6 @@
 /*
 MP3 audio decoder. Choice of public domain or MIT-0. See license statements at the end of this file.
-dr_mp3 - v0.6.7 - 2020-04-25
+dr_mp3 - v0.6.8 - TBD
 
 David Reid - mackron@gmail.com
 
@@ -4390,6 +4390,9 @@ counts rather than sample counts.
 /*
 REVISION HISTORY
 ================
+v0.6.8 - TBD
+  - Optimizations to decoding when initializing from memory.
+
 v0.6.7 - 2020-04-25
   - Fix a compilation error with DR_MP3_NO_STDIO
   - Optimization to decoding by reducing some data movement.
