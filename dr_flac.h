940bfadf39aee0009e52d54a4dc6eccf23940011
David Reid <mackron@gmail.com>
2020-08-02 11:39:54 +1000

    dr_flac: Version 0.12.17

--- dr_flac.h
+++ dr_flac.h
@@ -1,6 +1,6 @@
 /*
 FLAC audio decoder. Choice of public domain or MIT-0. See license statements at the end of this file.
-dr_flac - v0.12.17 - TBD
+dr_flac - v0.12.17 - 2020-08-02
 
 David Reid - mackron@gmail.com
 
@@ -11751,7 +11751,7 @@ DRFLAC_API drflac_bool32 drflac_next_cuesheet_track(drflac_cuesheet_track_iterat
 /*
 REVISION HISTORY
 ================
-v0.12.17 - TBD
+v0.12.17 - 2020-08-02
   - Simplify sized types.
 
 v0.12.16 - 2020-07-25
