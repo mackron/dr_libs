cff93216b5b9cd5a75567047392f69a71906c1cb
David Reid <mackron@gmail.com>
2020-08-30 11:25:50 +1000

    dr_flac: Version 0.12.19

--- dr_flac.h
+++ dr_flac.h
@@ -1,6 +1,6 @@
 /*
 FLAC audio decoder. Choice of public domain or MIT-0. See license statements at the end of this file.
-dr_flac - v0.12.19 - TBD
+dr_flac - v0.12.19 - 2020-08-30
 
 David Reid - mackron@gmail.com
 
@@ -232,7 +232,7 @@ extern "C" {
 
 #define DRFLAC_VERSION_MAJOR     0
 #define DRFLAC_VERSION_MINOR     12
-#define DRFLAC_VERSION_REVISION  18
+#define DRFLAC_VERSION_REVISION  19
 #define DRFLAC_VERSION_STRING    DRFLAC_XSTRINGIFY(DRFLAC_VERSION_MAJOR) "." DRFLAC_XSTRINGIFY(DRFLAC_VERSION_MINOR) "." DRFLAC_XSTRINGIFY(DRFLAC_VERSION_REVISION)
 
 #include <stddef.h> /* For size_t. */
@@ -11772,7 +11772,7 @@ DRFLAC_API drflac_bool32 drflac_next_cuesheet_track(drflac_cuesheet_track_iterat
 /*
 REVISION HISTORY
 ================
-v0.12.19 - TBD
+v0.12.19 - 2020-08-30
   - Fix a bug due to an undefined 32-bit shift.
 
 v0.12.18 - 2020-08-14
