f123d965abc1eeb27792eab541de58f4a9322062
David Reid <mackron@gmail.com>
2020-11-29 07:18:30 +1000

    dr_flac: Version 0.12.24

--- dr_flac.h
+++ dr_flac.h
@@ -1,6 +1,6 @@
 /*
 FLAC audio decoder. Choice of public domain or MIT-0. See license statements at the end of this file.
-dr_flac - v0.12.24 - TBD
+dr_flac - v0.12.24 - 2020-11-29
 
 David Reid - mackron@gmail.com
 
@@ -11792,8 +11792,8 @@ DRFLAC_API drflac_bool32 drflac_next_cuesheet_track(drflac_cuesheet_track_iterat
 /*
 REVISION HISTORY
 ================
-v0.12.24 - TBD
-  - Try fixing ARM64/NEON detection when compiling with MSVC.
+v0.12.24 - 2020-11-29
+  - Fix ARM64/NEON detection when compiling with MSVC.
 
 v0.12.23 - 2020-11-21
   - Fix compilation with OpenWatcom.
