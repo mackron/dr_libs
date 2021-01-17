0e308d1c92c2436fbcc6d2fa56f8e455fbfc6d48
David Reid <mackron@gmail.com>
2021-01-17 20:05:53 +1000

    dr_flac: Version 0.12.26

--- dr_flac.h
+++ dr_flac.h
@@ -1,6 +1,6 @@
 /*
 FLAC audio decoder. Choice of public domain or MIT-0. See license statements at the end of this file.
-dr_flac - v0.12.26 - TBD
+dr_flac - v0.12.26 - 2021-01-17
 
 David Reid - mackron@gmail.com
 
@@ -11805,7 +11805,7 @@ DRFLAC_API drflac_bool32 drflac_next_cuesheet_track(drflac_cuesheet_track_iterat
 /*
 REVISION HISTORY
 ================
-v0.12.26 - TBD
+v0.12.26 - 2021-01-17
   - Fix a compilation warning due to _BSD_SOURCE being deprecated.
 
 v0.12.25 - 2020-12-26
