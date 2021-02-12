dceddd8d6bcd8aabc84a1ad8bbf7b3e3fa54023d
G4Vi <hayesgav@gmail.com>
2021-02-11 23:21:49 -0500

    switched to use bit streaming

--- wip/dr_flac_matroska.c
+++ wip/dr_flac_matroska.c
@@ -1,17 +1,4 @@
 #include <stdio.h>
-/*
-static drflac_uint32 drflac_matroska_ebml_read_vint_width(drflac_uint64 vint) {
-    drflac_uint32 width;
-    unsigned i;
-
-    for(i = 0; i < (sizeof(vint)*8); i++) {
-        ++width; 
-        if(vint & 0x1) return width;
-        vint >>= 1;
-    }    
-    return 0;
-}
-*/
 
 static void drflac_matroska_print_hex(drflac_uint64 value) {
     drflac_uint8 thebyte;
@@ -24,50 +11,62 @@ static void drflac_matroska_print_hex(drflac_uint64 value) {
     printf("\n");    
 }
 
-static drflac_uint64 drflac_matroska_ebml_read_vint(drflac_read_proc onRead, void* pUserData) {
+
+static drflac_bool32 drflac_matroska_ebml_read_vint(drflac_bs *bs, drflac_uint64 *retval) {
     drflac_uint32 width;
     drflac_uint64 value = 0;
     drflac_uint8  thebyte;
-
-    if(onRead(pUserData, &thebyte, 1) == 1) {
-        /*printf("\nbyte 0x%X\n", thebyte);*/
-        /* loop through the VINT_WIDTH bits */
-        for(width = 1; width < 9; width++) {   
-            /* check for VINT_MARKER */                            
-            if(thebyte & (0x100 >> width)) {
-                /*printf("embl width %u\n", width);*/               
-                /* clear VINT_WIDTH and VINT_MARKER */
-                thebyte <<= width;
-                thebyte >>= width;                                
-                /* convert this byte and remaining bytes to an unsigned integer */
-                /* https://commandcenter.blogspot.com/2012/04/byte-order-fallacy.html */
-                do {
-                    width--;
-                    value |= (thebyte << (width*8));
-                    if(width == 0) break;
-                    onRead(pUserData, &thebyte, 1);
-                } while(1);
-
-                /*printf("embl value %u\n", value);
-                drflac_matroska_print_hex(value);*/
-                return value; 
-            }                                  
-        }
+	
+	if(!drflac__read_uint8(bs, 8, &thebyte)) return DRFLAC_FALSE;
+  
+    /*printf("\nbyte 0x%X\n", thebyte);*/
+    /* loop through the VINT_WIDTH bits */
+    for(width = 1; width < 9; width++) {   
+        /* check for VINT_MARKER */                            
+        if(thebyte & (0x100 >> width)) {
+            /*printf("embl width %u\n", width);*/               
+            /* clear VINT_WIDTH and VINT_MARKER */
+            thebyte <<= width;
+            thebyte >>= width;                                
+            /* convert this byte and remaining bytes to an unsigned integer */
+            /* https://commandcenter.blogspot.com/2012/04/byte-order-fallacy.html */
+            do {
+                width--;
+                value |= (thebyte << (width*8));
+                if(width == 0) {
+					/*printf("embl value %u\n", value);
+                    drflac_matroska_print_hex(value);*/
+					*retval = value;
+					return DRFLAC_TRUE;
+				}
+				if(!drflac__read_uint8(bs, 8, &thebyte)) return DRFLAC_FALSE;
+            } while(1);            
+        }                                  
     }
-    return 0;    
+    return DRFLAC_FALSE;
 }
 
-static drflac_bool32 drflac_matroska_ebml_id_and_size(drflac_read_proc onRead, void* pUserData, drflac_uint64 *id, drflac_uint64 *size, drflac_bool32 *sizeunknown) {
-    *id = drflac_matroska_ebml_read_vint(onRead, pUserData);    
-    *size = drflac_matroska_ebml_read_vint(onRead, pUserData);    
+static drflac_bool32 drflac_matroska_ebml_id_and_size(drflac_bs *bs, drflac_uint64 *id, drflac_uint64 *size, drflac_bool32 *sizeunknown) {
+    if(!drflac_matroska_ebml_read_vint(bs, id)) return DRFLAC_FALSE;
+    if(!drflac_matroska_ebml_read_vint(bs, size)) return DRFLAC_FALSE;   
     *sizeunknown = DRFLAC_FALSE;
     return DRFLAC_TRUE;
 }
 
-static drflac_bool32 drflac_matroska_ebml_read_unsigned(drflac_read_proc onRead, void* pUserData, size_t bytelen, drflac_uint64 *value) {
+static drflac_bool32 drflac_bs_read_bytearray(drflac_bs *bs, size_t bytelen, drflac_uint8* result) {
+	int i;
+	for(i = 0; i < bytelen; i++) {
+		if( ! drflac__read_uint8(bs, 8, &result[i])) return DRFLAC_FALSE;		
+	}
+	return DRFLAC_TRUE;
+}
+
+static drflac_bool32 drflac_matroska_ebml_read_unsigned(drflac_bs *bs, size_t bytelen, drflac_uint64 *value) {
     drflac_uint8 element[8];
     int i;
-    if( ! onRead(pUserData, element, bytelen) == bytelen) return DRFLAC_FALSE;
+	
+	if(!drflac_bs_read_bytearray(bs, bytelen, element)) return DRFLAC_FALSE;
+	
     *value = 0;
     for( i = 0; i < bytelen; i++) {
         *value |= (element[i] << ((bytelen-i-1)*8));
@@ -75,9 +74,9 @@ static drflac_bool32 drflac_matroska_ebml_read_unsigned(drflac_read_proc onRead,
     return DRFLAC_TRUE;
 }
 
-static drflac_bool32 drflac_matroska_ebml_read_string(drflac_read_proc onRead, void* pUserData, size_t bytelen, char *string) {
-    if( ! onRead(pUserData, string, bytelen) == bytelen) return DRFLAC_FALSE;
-    string[bytelen] = '\0';
+static drflac_bool32 drflac_matroska_ebml_read_string(drflac_bs *bs, size_t bytelen, char *string) {
+	if(!drflac_bs_read_bytearray(bs, bytelen, string)) return DRFLAC_FALSE;
+	string[bytelen] = '\0';
     return DRFLAC_TRUE;
 }
 
@@ -93,39 +92,40 @@ static drflac_bool32 drflac__init_private__matroska(drflac_init_info* pInit, drf
     int i;
     (void)relaxed;
     /* master element size*/
-    drflac_matroska_ebml_read_vint(onRead, pUserData);
+	if(!drflac_matroska_ebml_read_vint(&pInit->bs, &content)) return DRFLAC_FALSE;
+	printf("master element size %u\n", content);
     for(;;) {
-        drflac_matroska_ebml_id_and_size(onRead, pUserData, &id, &size, &size_unknown);
+        drflac_matroska_ebml_id_and_size(&pInit->bs, &id, &size, &size_unknown);
         printf("ebml size %u\n", size);
           
         switch(id) {
             case 646:
-            drflac_matroska_ebml_read_unsigned(onRead, pUserData, size, &content);
+            drflac_matroska_ebml_read_unsigned(&pInit->bs, size, &content);
             printf("ebml version %u\n", content);
             break;
             case 759:
-            drflac_matroska_ebml_read_unsigned(onRead, pUserData, size, &content);
+            drflac_matroska_ebml_read_unsigned(&pInit->bs, size, &content);
             printf("ebml read version %u\n", content);
             break;
             case 754:
-            drflac_matroska_ebml_read_unsigned(onRead, pUserData, size, &content);            
+            drflac_matroska_ebml_read_unsigned(&pInit->bs, size, &content);            
             printf("ebml max ebml id len %u\n", content);
             break;
             case 755:
-            drflac_matroska_ebml_read_unsigned(onRead, pUserData, size, &content);
+            drflac_matroska_ebml_read_unsigned(&pInit->bs, size, &content);
             printf("ebml max ebml size len %u\n", content);
             break;
             case 642:
             if(size >= sizeof(string)) return DRFLAC_FALSE;
-            drflac_matroska_ebml_read_string(onRead, pUserData, size, string);
+            drflac_matroska_ebml_read_string(&pInit->bs, size, string);
             printf("ebml document type %s\n", string);
             break;
             case 647:
-            drflac_matroska_ebml_read_unsigned(onRead, pUserData, size, &content);
+            drflac_matroska_ebml_read_unsigned(&pInit->bs, size, &content);
             printf("ebml document version %u\n", content);
             break;
             case 645:
-            drflac_matroska_ebml_read_unsigned(onRead, pUserData, size, &content);
+            drflac_matroska_ebml_read_unsigned(&pInit->bs, size, &content);
             printf("ebml document read version %u\n", content);
             break;            
             default:
@@ -134,7 +134,7 @@ static drflac_bool32 drflac__init_private__matroska(drflac_init_info* pInit, drf
             if(size > 8) {
                 return DRFLAC_FALSE;
             }
-            onRead(pUserData, &content, size);
+			if(!drflac_bs_read_bytearray(&pInit->bs, size, &content)) return DRFLAC_FALSE;
             printf("ebml as hex 0x"); 
             for(i = 0; i < size; i++) {
                 thebyte = content >> (i*8);
