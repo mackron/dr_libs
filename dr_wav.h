61fcf380728dc7fc7b781aae058b44055831b3e7
David Reid <mackron@gmail.com>
2021-07-31 10:01:14 +1000

    dr_wav: Version 0.13.1

--- dr_wav.h
+++ dr_wav.h
@@ -1,6 +1,6 @@
 /*
 WAV audio loader and writer. Choice of public domain or MIT-0. See license statements at the end of this file.
-dr_wav - v0.13.1 - TBD
+dr_wav - v0.13.1 - 2021-07-31
 
 David Reid - mackron@gmail.com
 
@@ -7525,7 +7525,7 @@ DRWAV_API drwav_bool32 drwav_fourcc_equal(const drwav_uint8* a, const char* b)
 /*
 REVISION HISTORY
 ================
-v0.13.1 - TBD
+v0.13.1 - 2021-07-31
   - Fix platform detection for ARM64.
 
 v0.13.0 - 2021-07-01
