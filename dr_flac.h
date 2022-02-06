3c4758eff403d79bc997541251ccf7a4ac6f22a8
David Reid <mackron@gmail.com>
2022-02-06 20:52:27 +1000

    dr_flac: Update revision history.

--- dr_flac.h
+++ dr_flac.h
@@ -1,6 +1,6 @@
 /*
 FLAC audio decoder. Choice of public domain or MIT-0. See license statements at the end of this file.
-dr_flac - v0.12.35 - 2022-02-06
+dr_flac - v0.12.36 - TBA
 
 David Reid - mackron@gmail.com
 
@@ -232,7 +232,7 @@ extern "C" {
 
 #define DRFLAC_VERSION_MAJOR     0
 #define DRFLAC_VERSION_MINOR     12
-#define DRFLAC_VERSION_REVISION  35
+#define DRFLAC_VERSION_REVISION  36
 #define DRFLAC_VERSION_STRING    DRFLAC_XSTRINGIFY(DRFLAC_VERSION_MAJOR) "." DRFLAC_XSTRINGIFY(DRFLAC_VERSION_MINOR) "." DRFLAC_XSTRINGIFY(DRFLAC_VERSION_REVISION)
 
 #include <stddef.h> /* For size_t. */
@@ -11938,6 +11938,9 @@ DRFLAC_API drflac_bool32 drflac_next_cuesheet_track(drflac_cuesheet_track_iterat
 /*
 REVISION HISTORY
 ================
+v0.12.36 - TBA
+  - Fix a compilation error with the ARM build.
+
 v0.12.35 - 2022-02-06
   - Fix a bug due to underestimating the amount of precision required for the prediction stage.
   - Fix some bugs found from fuzz testing.
