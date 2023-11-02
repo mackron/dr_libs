9eed1be421749ba68a87e5b4c3b10858f8580689
David Reid <mackron@gmail.com>
2023-11-02 14:27:12 +1000

    dr_wav: Version 0.13.13

--- dr_wav.h
+++ dr_wav.h
@@ -1,6 +1,6 @@
 /*
 WAV audio loader and writer. Choice of public domain or MIT-0. See license statements at the end of this file.
-dr_wav - v0.13.13 - TBD
+dr_wav - v0.13.13 - 2023-11-02
 
 David Reid - mackron@gmail.com
 
@@ -8347,7 +8347,7 @@ DRWAV_API drwav_bool32 drwav_fourcc_equal(const drwav_uint8* a, const char* b)
 /*
 REVISION HISTORY
 ================
-v0.13.13 - TBD
+v0.13.13 - 2023-11-02
   - Fix a warning when compiling with Clang.
 
 v0.13.12 - 2023-08-07
