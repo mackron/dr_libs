a1dd66cb680522a753bac7dff306b4a1cfd75e26
David Reid <mackron@gmail.com>
2024-12-17 09:49:14 +1000

    dr_flac: Version 0.12.43

--- dr_flac.h
+++ dr_flac.h
@@ -1,6 +1,6 @@
 /*
 FLAC audio decoder. Choice of public domain or MIT-0. See license statements at the end of this file.
-dr_flac - v0.12.43 - TBD
+dr_flac - v0.12.43 - 2024-12-17
 
 David Reid - mackron@gmail.com
 
@@ -12095,7 +12095,7 @@ DRFLAC_API drflac_bool32 drflac_next_cuesheet_track(drflac_cuesheet_track_iterat
 /*
 REVISION HISTORY
 ================
-v0.12.43 - TBD
+v0.12.43 - 2024-12-17
   - Fix a possible buffer overflow during decoding.
   - Improve detection of ARM64EC
 
