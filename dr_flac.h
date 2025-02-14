3a840bd4f9261ea62d8d96a394152dd45c484f7f
Ranjit Ranjan <ranjit.ranjan@ibm.com>
2025-02-14 02:39:58 -0600

    Addressing review comments

--- dr_flac.h
+++ dr_flac.h
@@ -8294,8 +8294,8 @@ static drflac_result drflac_result_from_errno(int e)
     #ifdef ENOSYS
         case ENOSYS: return DRFLAC_NOT_IMPLEMENTED;
     #endif
-    //In AIX, ENOTEMPTY and EEXIST uses the same value
-    #if defined(ENOTEMPTY) && ENOTEMPTY!=EEXIST
+    /*In AIX, ENOTEMPTY and EEXIST uses the same value*/
+    #if defined(ENOTEMPTY) && ENOTEMPTY != EEXIST
         case ENOTEMPTY: return DRFLAC_DIRECTORY_NOT_EMPTY;
     #endif
     #ifdef ELOOP
