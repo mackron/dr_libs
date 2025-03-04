5daa1859146340c4ec56172c89428ce033211848
David Reid <mackron@gmail.com>
2025-03-04 12:12:23 +1000

    dr_mp3: Add a new test.
    This extracts the raw data to a file. Not really intended to be used as
    an automatic test just yet, but I might integrate minimp3_ex to use as
    a benchmark since that should be bit-exact with dr_mp3.

---
+++ tests/mp3/mp3_extract.c
@@ -0,0 +1,83 @@
+#include "mp3_common.c"
+
+#define FORMAT_S16 0
+#define FORMAT_F32 1
+
+int main(int argc, char** argv)
+{
+    drmp3_result result;
+    drmp3 mp3;
+    const char* pOutputFilePath = NULL;
+    int format = FORMAT_S16;
+    int iarg;
+    FILE* pFileOut;
+    drmp3_uint64 framesRead;
+
+    if (argc < 2) {
+        printf("Usage: mp3_extract <input filename> -o <output filename> -f [s16|f32]\n");
+        return 1;
+    }
+
+    if (drmp3_init_file(&mp3, argv[1], NULL) == DRMP3_FALSE) {
+        printf("Failed to open file: %s\n", argv[1]);
+        return 1;
+    }
+
+    /* Parse parameters. */
+    for (iarg = 2; iarg < argc; iarg += 1) {
+        if (strcmp(argv[iarg], "-o") == 0) {
+            if (iarg+1 < argc) {
+                iarg += 1;
+                pOutputFilePath = argv[iarg];
+            }
+        } else if (strcmp(argv[iarg], "-f") == 0) {
+            if (iarg+1 < argc) {
+                iarg += 1;
+                if (strcmp(argv[iarg], "s16") == 0) {
+                    format = FORMAT_S16;
+                } else if (strcmp(argv[iarg], "f32") == 0) {
+                    format = FORMAT_F32;
+                }
+            }
+        }
+    }
+
+    if (pOutputFilePath == NULL) {
+        printf("No output file specified.\n");
+        return 1;
+    }
+
+    if (drmp3_fopen(&pFileOut, pOutputFilePath, "wb") != DRMP3_SUCCESS) {
+        printf("Failed to open output file: %s\n", pOutputFilePath);
+        return 1;
+    }
+
+    if (format == FORMAT_S16) {
+        drmp3_int16 pcm[4096];
+
+        for (;;) {
+            framesRead = drmp3_read_pcm_frames_s16(&mp3, sizeof(pcm)/sizeof(pcm[0]) / mp3.channels, pcm);
+            if (framesRead == 0) {
+                break;
+            }
+
+            fwrite(pcm, 1, framesRead * mp3.channels * sizeof(pcm[0]), pFileOut);
+        }
+    } else {
+        float pcm[4096];
+
+        for (;;) {
+            framesRead = drmp3_read_pcm_frames_f32(&mp3, sizeof(pcm)/sizeof(pcm[0]) / mp3.channels, pcm);
+            if (framesRead == 0) {
+                break;
+            }
+
+            fwrite(pcm, 1, framesRead * mp3.channels * sizeof(pcm[0]), pFileOut);
+        }
+    }
+
+    fclose(pFileOut);
+    drmp3_uninit(&mp3);
+
+    return 0;
+}
