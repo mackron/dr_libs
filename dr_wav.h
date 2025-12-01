877b0967ce679148f60d69bb2d9173487717d8de
David Reid <mackron@gmail.com>
2025-12-02 05:29:09 +1000

    dr_wav: Version 0.14.2

--- dr_wav.h
+++ dr_wav.h
@@ -1,6 +1,6 @@
 /*
 WAV audio loader and writer. Choice of public domain or MIT-0. See license statements at the end of this file.
-dr_wav - v0.14.2 - TBD
+dr_wav - v0.14.2 - 2025-12-02
 
 David Reid - mackron@gmail.com
 
@@ -147,7 +147,7 @@ extern "C" {
 
 #define DRWAV_VERSION_MAJOR     0
 #define DRWAV_VERSION_MINOR     14
-#define DRWAV_VERSION_REVISION  1
+#define DRWAV_VERSION_REVISION  2
 #define DRWAV_VERSION_STRING    DRWAV_XSTRINGIFY(DRWAV_VERSION_MAJOR) "." DRWAV_XSTRINGIFY(DRWAV_VERSION_MINOR) "." DRWAV_XSTRINGIFY(DRWAV_VERSION_REVISION)
 
 #include <stddef.h> /* For size_t. */
@@ -8517,7 +8517,7 @@ DRWAV_API drwav_bool32 drwav_fourcc_equal(const drwav_uint8* a, const char* b)
 /*
 REVISION HISTORY
 ================
-v0.14.2 - TBD
+v0.14.2 - 2025-12-02
   - Fix a compilation warning.
 
 v0.14.1 - 2025-09-10
